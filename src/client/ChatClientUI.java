b5ff17fb39e52c4e88642ba4124aa2245447cd88
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2024-02-26 12:19:14 -0600

    code base

---
+++ src/client/ChatClientUI.java
@@ -0,0 +1,56 @@
+package client;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ChatClientUI {
+    private JFrame frame;
+    private JTextArea chatArea;
+    private JTextField messageField;
+    private JButton sendButton;
+    private UDPClient client;
+
+    public ChatClientUI(UDPClient client) {
+        this.client = client;
+        createUI();
+    }
+
+    private void createUI() {
+        frame = new JFrame("Chat Client");
+        chatArea = new JTextArea(20, 30);
+        chatArea.setEditable(false);
+        messageField = new JTextField(25);
+        sendButton = new JButton("Send");
+
+        JPanel panel = new JPanel();
+        panel.add(new JScrollPane(chatArea));
+        panel.add(messageField);
+        panel.add(sendButton);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        sendButton.addActionListener((ActionListener) new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = messageField.getText();
+                client.sendMessage(message);
+                messageField.setText("");
+            }
+        });
+    }
+
+    public void displayMessage(String message) {
+        chatArea.append(message + "\n");
+    }
+}
